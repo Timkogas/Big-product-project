6aa78d50c09c4350b54f5d1f6faffb09435d92b0
Timur Ermolaev <timkogas@gmail.com>
2023-02-10 20:23:55 +0300

    Настроил абслютные импорты, а также начал внедрять Feature-Sliced Design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import { Suspense, useContext, useState } from "react";
+import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import Counter from "./components/Counter";
 import './styles/index.scss'
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { useTheme } from "./providers/ThemeProvider";
 
 export enum Theme {
   LIGHT = 'light',
@@ -14,7 +13,7 @@ export enum Theme {
 }
 
 function App() {
-  const {theme, toggleTheme} = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
@@ -22,11 +21,10 @@ function App() {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading....</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />} />
-          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
-      <Counter />
     </div>
   );
 }
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 import { FC, useMemo, useState } from "react";
-import { Theme } from "../App";
-import { LOCAL_STORAGE_THEME_KEY, ThemeContext } from "./themeContext";
+import { Theme } from "../../../App";
+import { LOCAL_STORAGE_THEME_KEY, ThemeContext } from "../lib/ThemeContext";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
 
