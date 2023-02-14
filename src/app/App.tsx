9041c7ffea7b838758628198fb365270244b3586
Timur Ermolaev <timkogas@gmail.com>
2023-02-14 16:32:47 +0300

    Реализовал первый UI kit компонент, начал реализацию навбара

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
 import './styles/index.scss'
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 export enum Theme {
   LIGHT = 'light',
@@ -13,9 +13,8 @@ function App() {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar/>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
       <AppRouter/>
     </div>
   );
