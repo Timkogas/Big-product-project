035dee4210c4a1662eb3931000bbb6dd431b758b
Timur Ermolaev <timkogas@gmail.com>
2023-02-14 18:18:15 +0300

    Настроил лоадеры вебпака для работы с разными медиа-файлами, реализовал свитчер темы

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,11 +10,10 @@ export enum Theme {
 }
 
 function App() {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar/>
-      <button onClick={toggleTheme}>TOGGLE</button>
       <AppRouter/>
     </div>
   );
