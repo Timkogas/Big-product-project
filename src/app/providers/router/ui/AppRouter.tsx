876b6be151b5cb6c9cd679db8b0b38cb768e7924
Timur Ermolaev <timkogas@gmail.com>
2023-02-18 18:15:46 +0300

    Добавил лоудер и обработку 404 страницы

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,25 +1,26 @@
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgets/PageLoader'
+
 
 function AppRouter () {
   return (
 
-      <Suspense fallback={''}>
-          <Routes>
-              {Object.values(routeConfig).map(({ path, element }) => (
-                  <Route
-                      key={path}
-                      path={path}
-                      element={
+      <Routes>
+          {Object.values(routeConfig).map(({ path, element }) => (
+              <Route
+                  key={path}
+                  path={path}
+                  element={
+                      <Suspense fallback={<PageLoader/>}>
                           <div className='page-wrapper'>
                               {element}
                           </div>
-
+                      </Suspense>
                         }
-                    />))}
-          </Routes>
-      </Suspense>
+                />))}
+      </Routes>
 
   )
 }
